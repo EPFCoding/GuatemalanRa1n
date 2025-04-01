b81074c9a030a52a5b2a8416acc7abb8d6a91f50
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2025-04-01 16:30:48 +0800

    Update Zebra Info.plist to have sileo url scheme

--- Meridian/Meridian/views/ViewController.m
+++ Meridian/Meridian/views/ViewController.m
@@ -25,7 +25,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *versionLabel;
 @end
 
-NSString *Version = @"MeridianFix: v0.9-009";
+NSString *Version = @"MeridianFix: v0.10.0";
 NSOperatingSystemVersion osVersion;
 
 id thisClass;
